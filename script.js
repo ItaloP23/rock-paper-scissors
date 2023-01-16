d7c009c41b4ee923db854404144b3de96ff7fa91
ItaloP23 <ita_5@msn.com>
2023-01-16 11:11:57 +0100

    First commit of the project

---
+++ script.js
@@ -0,0 +1,66 @@
+const getComputerChoice = () => {
+    // ritorna la scelta del computer
+    const CHOICES = ["rock" , "paper" , "scissors" ];
+    const INDEX = Math.floor( Math.random() * 3);
+    return CHOICES[INDEX]
+}
+
+const getPlayerSelection = () => {
+    // prompt una scelta del giocatore e la ritorna
+    let playerChoice;
+    let check = false;
+    while (!check) {
+        playerChoice = prompt("Player choses:" , )
+        if (/^rock$|^paper$|^scissors$/i.test(playerChoice)) {
+            check = true;
+        } else { console.log("Enter rock, paper or scissors") }
+    }
+    return playerChoice
+}
+
+const playRound = ( computer , player ) => {
+    //choices
+    console.log(`Computer chooses ${computer}, player chooses ${player}`)
+
+    // who wins between computer and player    
+    if ( computer[0] === player[0] ) {
+        console.log("it's a tie");
+        computerScore++;
+        playerScore++;
+        return
+    } else if (
+        ( computer[0] === 'r' && player[0] === ( 's' || 'S' ) ) ||
+        ( computer[0] === 's' && player[0] === ( 'p' || 'P' ) ) ||
+        ( computer[0] === 'p' && player[0] === ( 'r' || 'R' ) )
+    )  {
+        console.log("computer wins");
+        computerScore++;
+        return
+    } else {
+        console.log("player wins");
+        playerScore++;
+        return 
+    }
+
+}
+
+const game = ( numberOfGames ) => {
+
+    let computerScore = 0;
+    let playerScore = 0;
+    
+    // for loop over the number of games
+    for (let i = 1 ; i <= numberOfGames ; i++) {
+
+        //call a single round play
+        playRound( getComputerChoice() , getPlayerSelection() )
+        
+        //keeping track of the scores
+        console.log(`Games number ${i} - Live score: Computer ${computerScore} - ${playerScore} Player`);
+    }
+}
+
+
+
+
+
