d736a2cd03a0699da58f90d15446593b3b0c8873
ItaloP23 <ita_5@msn.com>
2023-01-16 12:19:24 +0100

    Put playRound funct inside game funct

--- script.js
+++ script.js
@@ -18,36 +18,41 @@ const getPlayerSelection = () => {
     return playerChoice
 }
 
-const playRound = ( computer , player ) => {
-    //choices
-    console.log(`Computer chooses ${computer}, player chooses ${player}`)
-
-    // who wins between computer and player    
-    if ( computer[0] === player[0] ) {
-        console.log("it's a tie");
-        computerScore++;
-        playerScore++;
-        return
-    } else if (
-        ( computer[0] === 'r' && player[0] === ( 's' || 'S' ) ) ||
-        ( computer[0] === 's' && player[0] === ( 'p' || 'P' ) ) ||
-        ( computer[0] === 'p' && player[0] === ( 'r' || 'R' ) )
-    )  {
-        console.log("computer wins");
-        computerScore++;
-        return
-    } else {
-        console.log("player wins");
-        playerScore++;
-        return 
-    }
-
+const printResult = (winner) => {
+    console.log("Game is finished, " + winner + " won!")
 }
+
+
 
 const game = ( numberOfGames ) => {
 
-    let computerScore = 0;
     let playerScore = 0;
+    let computerScore = 0;
+
+    const playRound = ( computer , player ) => {
+        //choices
+        console.log(`Computer chooses ${computer}, player chooses ${player}`)
+    
+        // who wins between computer and player    
+        if ( computer[0] === player[0] ) {
+            console.log("it's a tie");
+            computerScore++;
+            playerScore++;
+            return
+        } else if (
+            ( computer[0] === 'r' && player[0] === ( 's' || 'S' ) ) ||
+            ( computer[0] === 's' && player[0] === ( 'p' || 'P' ) ) ||
+            ( computer[0] === 'p' && player[0] === ( 'r' || 'R' ) )
+        )  {
+            console.log("computer wins");
+            computerScore++;
+            return
+        } else {
+            console.log("player wins");
+            playerScore++;
+            return 
+        }
+    }
     
     // for loop over the number of games
     for (let i = 1 ; i <= numberOfGames ; i++) {
@@ -56,11 +61,19 @@ const game = ( numberOfGames ) => {
         playRound( getComputerChoice() , getPlayerSelection() )
         
         //keeping track of the scores
-        console.log(`Games number ${i} - Live score: Computer ${computerScore} - ${playerScore} Player`);
+        console.log(`Game number ${i} - Live score: Computer ${computerScore} - ${playerScore} Player`);
     }
+
+    if ( computerScore === playerScore) {
+        console.log("The game ends with a tie");
+        return
+    } 
+
+    computerScore > playerScore ? printResult("computer") : printResult("player");
+    return
+
+
+    
 }
 
-
-
-
-
+game(5);
